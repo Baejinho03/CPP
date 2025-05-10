4bc6d54c465263b040b1eb642eff48cc0362c245
Baejinho03 <wlsghz69@gmail.com>
2025-05-10 22:44:32 +0900

    Add files via upload

---
+++ [6]세_정수형_변수_선언.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int a = 7; // 변수 입력
+	int b = 23;
+	int c = 30;
+
+	cout << a << " " << "+" << " " << b << " " << "=" << " " << c; // " 7 + 23 = 30 " 출력
+
+	return 0;
+}
