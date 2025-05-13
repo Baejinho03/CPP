282bf6f698fa63418574cff1c783fc29d2a2f67a
Baejinho03 <wlsghz69@gmail.com>
2025-05-13 22:09:23 +0900

    Update and rename [6]세_정수형_변수_선언.cpp to [06]세_정수형_변수_선언.cpp

---
+++ [06]세_정수형_변수_선언.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int a = 7; // º¯¼ö ÀÔ·Â
+	int b = 23;
+	int c = 30;
+
+	cout << a << " " << "+" << " " << b << " " << "=" << " " << c; // " 7 + 23 = 30 " Ãâ·Â
+
+	return 0;
+}
