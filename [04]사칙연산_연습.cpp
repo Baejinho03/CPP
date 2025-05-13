356da88c05f3107b42b283c0fe35bf7511b77a3c
Baejinho03 <wlsghz69@gmail.com>
2025-05-13 22:09:05 +0900

    Update and rename [4]사칙연산_연습.cpp to [04]사칙연산_연습.cpp

---
+++ [04]사칙연산_연습.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a = 5; // a 에 5 대입
+	int b = 30; // b 에 30 대입
+	int c = a + b; // c 에 a + b 대입
+
+	cout << c / 8 << "\n" << c % 8; // c를 8로 나눈 몫과 나머지 출력
+
+	return 0;
+}
