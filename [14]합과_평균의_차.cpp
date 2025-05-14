20229162fb6f9bcf6de444ec54507e7c80667e14
Baejinho03 <wlsghz69@gmail.com>
2025-05-14 21:11:49 +0900

    Add files via upload

---
+++ [14]합과_평균의_차.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+
+	cin >> a >> b >> c; // 입력
+
+	cout << a + b + c << endl << (a + b + c) / 3 << endl << (a + b + c) - ((a + b + c) / 3); // 합 , 평균, 합과 평균의 차 출력
+
+	return 0;
+}
