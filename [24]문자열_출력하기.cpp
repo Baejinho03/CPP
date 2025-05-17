d53e885d9b4652122574a99cc58d0187dfeb90fb
Baejinho03 <wlsghz69@gmail.com>
2025-05-17 22:40:43 +0900

    Add files via upload

---
+++ [24]문자열_출력하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	
+	cin >> n;
+
+	for (int i = 0; i < n; i++) { // 입력받은 n 값만큼 "LeebrosCode" 출력
+		cout << "LeebrosCode" << endl;
+	}
+
+	return 0;
+}
