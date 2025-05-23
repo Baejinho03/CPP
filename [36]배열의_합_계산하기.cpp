207d29cb8aa2a96ac2b0ffba7af9edb4a879cc6d
Baejinho03 <wlsghz69@gmail.com>
2025-05-23 22:55:12 +0900

    Add files via upload

---
+++ [36]배열의_합_계산하기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a[5] = { 3, 1, 4, 5, 9 }; // 배열 a 선언
+	
+	cout << a[1] + a[3] + a[4]; // 인덱스가 1, 3, 4인 배열 a의 원소를 더한 값 출력
+
+	return 0;
+}
